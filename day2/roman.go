25c0821e0d8c063f6ec1566bbc9db0a43b1033fa
VishakhaSainani <vishakhasainani26@gmail.com>
2025-01-14 17:37:28 +0530

    Worked on assignment - convert roman to integer

---
+++ day2/roman.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+)
+
+func integer_value(roman_letter byte ) int{
+	m:=map[byte]int{
+		'I':1,
+		'V':5,
+		'X':10,
+		'L':50,
+		'C':100,
+		'D':500,
+		'M':1000,
+	}
+	return m[roman_letter]
+	
+}
+
+func convert_roman_to_integer(roman string) int{
+	var result int
+	for i:=0;i<len(roman);i++ {
+		s1:=integer_value(roman[i]);
+		if i+1<len(roman) {
+			s2:=integer_value(roman[i+1])
+
+			if s2<=s1{
+				result+=s1
+			} else {
+				result+=s2-s1
+				i+=1
+			}
+		} else {
+			result+=s1
+		}
+	}
+	return result
+}
+
+func main(){
+	
+	var roman_val string
+	fmt.Scanf("%s",&roman_val)
+
+	result:=convert_roman_to_integer(roman_val)
+
+	fmt.Println(result)
+
+}
