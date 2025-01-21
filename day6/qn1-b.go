267a54e63360d7561f52a4eff6b51343be4c5f9d
VishakhaSainani <vishakhasainani26@gmail.com>
2025-01-21 18:10:57 +0530

    Channel program using select

---
+++ day6/qn1-b.go
@@ -0,0 +1,54 @@
+/*1. Given a string containing conversation between alice and bob. In the string, if it reaches $, it means end of alice message and if it reaches #, it means end of bob's message. and if it reaches ^,
+it means end of conversation ignore string after that.
+
+Note: given string doesn't contain any spaces. If message contains two continuous conversations from one person it should be printed one after another as given in the example.
+
+write a program to separate out messages from alice and bob. Write messages from alice and bob on seperate channels, whenever a message from alice/bob, print it in front of their name as shown in the example below:
+
+Note: there is a single space before and after colon(:) and no space before and after comma.
+
+e.g: "helloBob$helloalice#howareyou?#Iamgood.howareyou?$^"
+output: alice : helloBob,bob : helloalice,bob : howareyou?,alice : Iamgood.howareyou?*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var msg string = "helloBob$helloalice#howareyou?#Iamgood.howareyou?$^"
+	var dialogue string
+	alice := make(chan string)
+	bob := make(chan string)
+	go func() {
+		for {
+			select {
+			case aliceMsg := <-alice:
+				fmt.Printf("alice : %s,", aliceMsg)
+
+			case bobMsg := <-bob:
+				fmt.Printf("bob : %s,", bobMsg)
+			}
+		}
+	}()
+
+	for _, i := range msg {
+		if i == '$' {
+			alice <- dialogue
+			dialogue = ""
+			continue
+		} else if i == '#' {
+			bob <- dialogue
+			dialogue = ""
+			continue
+		} else if i == '^' {
+			break
+		}
+		dialogue = dialogue + string(i)
+	}
+
+	close(alice)
+	close(bob)
+
+}
