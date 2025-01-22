f4d63b8fb57eaf371a20b52192cf42008b2454fe
VishakhaSainani <vishakhasainani26@gmail.com>
2025-01-22 18:44:08 +0530

    Handle goroutine race condition Program

---
+++ day7/qn1.go
@@ -0,0 +1,53 @@
+/*Assignment:
+In the below code snippet concurrent goroutines execution corrupts a piece of data by
+accessing it simultaneously it leads in raise condition.
+https://go.dev/play/p/SmLMf8hZQzs
+output when you run this : 1 is Even (may vary when you run multiple times)
+
+Update the given code to print the correct output.
+output once code is corrected: 0 is Even*/
+
+//The follwoing code was given
+//Updates made: Added MUtex to acquire lock to critical section
+
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func isEven(n int) bool {
+	return n%2 == 0
+}
+
+var mutex sync.Mutex
+
+func main() {
+
+	n := 0
+	go func() {
+		mutex.Lock()
+
+		nIsEven := isEven(n)
+
+		time.Sleep(5 * time.Millisecond)
+		if nIsEven {
+			fmt.Println(n, " is even")
+			return
+		}
+		fmt.Println(n, "is odd")
+		mutex.Unlock()
+
+	}()
+	go func() {
+		mutex.Lock()
+
+		n++
+		mutex.Unlock()
+	}()
+
+	// just waiting for the goroutines to finish before exiting
+	time.Sleep(time.Second)
+}
